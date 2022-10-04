e0ef356818fde330c7a8c7d603341e59ed1b0e48
Heather <lilpinkrose02@gmail.com>
2022-10-03 21:15:51 -0500

    made images as close to the same size as possible. Some have whitespace whithin the image and will not size correctly.

--- script.js
+++ script.js
@@ -5,15 +5,45 @@ let foodSearch
 
 searchBtn.addEventListener('click', () => {
     console.log(searchBar.value)
-    searchVar  = searchBar.value
+    searchVar = searchBar.value
     foodSearch = `https://api.spoonacular.com/recipes/search?query=${searchVar}&apiKey=09f3d9d95f864ab0a9218e1fb719c9a4`
     fetchData(foodSearch)
 })
 
-const fetchData = (url) => fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-        console.log('data is loaded')
-        console.log(data)
+// const fetchData = (url) => fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => {
+//         console.log('data is loaded')
+//         console.log(data)
+//     })
+
+
+const fetchData = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+    // return data
+    console.log(data)
+
+    const totalResults = Array.from(data.results)
+    console.log(totalResults)
+
+    totalResults.forEach(result => {
+        const newSection = document.createElement('section')
+        const newHeader = document.createElement('h1')
+        const newDiv = document.createElement('div')
+        const newImg = document.createElement('img')
+
+        document.querySelector('body').appendChild(newSection)
+        newSection.appendChild(newHeader)
+        newSection.appendChild(newDiv)
+        newDiv.appendChild(newImg)
+
+        newSection.classList.add('result-wrap')
+        newDiv.classList.add('image-wrap')
+        newHeader.innerText = result.title
+        newImg.src = ` https://spoonacular.com/recipeImages/${result.image}`
+        // newDiv.style.backgroundImage = `url(https://spoonacular.com/recipeImages/${result.image})`
     })
 
+    
+}
